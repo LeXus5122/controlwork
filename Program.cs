931eea8daf719be9b8c0c7aeefed69ac696095ca
LeXus22 <lexus22@inbox.ru>
2023-04-18 10:49:51 +0300

    final var -rabochiy

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
-﻿string[] inputArray = new string[] { "Hello", "2", "world", ":-)" };
-int arraySize = inputArray.Length;
-    string[] userInputArray = CreateUserArray(arraySize);
-    Array.Copy(inputArray, userInputArray, arraySize);
-    Console.WriteLine("Входной массив:");
-    DisplayArray(userInputArray);
-    int finalArraySize = CalculateResultArraySize(userInputArray);
-    string[] finalArray = CreateUserArray(finalArraySize);
-    FillResultantArray(userInputArray, finalArray);
-    Console.WriteLine("Результирующий массив:");
-    DisplayArray(finalArray);
-
+﻿Console.WriteLine("Введите элементы массива (через запятую):");
+string[] userInputArray = PopulateArray();
+Console.WriteLine("Входной массив:");
+DisplayArray(userInputArray);
+int finalArraySize = CalculateResultArraySize(userInputArray);
+string[] finalArray = CreateUserArray(finalArraySize);
+FillResultantArray(userInputArray, finalArray);
+Console.WriteLine("Результирующий массив:");
+DisplayArray(finalArray);
 
 static string[] CreateUserArray(int size)
 {
@@ -17,36 +14,49 @@ static string[] CreateUserArray(int size)
     return newArray;
 }
 
-static void DisplayArray(string[] array)
+static string[] PopulateArray()
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    string inputLine = Console.ReadLine()!;
+    string[] elements = inputLine.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+    for (int j = 0; j < elements.Length; j++)
     {
-        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
-        else Console.Write($"{array[i]}]");
+        elements[j] = elements[j].Trim();
     }
-    Console.WriteLine();
+
+    return elements;
 }
+
+static void DisplayArray(string[] array)
+    {
+        Console.Write("[");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}]");
+        }
+        Console.WriteLine();
+    }
 
 static int CalculateResultArraySize(string[] array)
-{
-    int resultSize = 0;
-    foreach (string element in array)
     {
-        if (element.Length <= 3) resultSize++;
+        int resultSize = 0;
+        foreach (string element in array)
+        {
+            if (element.Length <= 3) resultSize++;
+        }
+        return resultSize;
     }
-    return resultSize;
-}
 
 static void FillResultantArray(string[] initialArray, string[] finalArray)
-{
-    int resultIndex = 0;
-    foreach (string element in initialArray)
     {
-        if (element.Length <= 3)
+        int resultIndex = 0;
+        foreach (string element in initialArray)
         {
-            finalArray[resultIndex] = element;
-            resultIndex++;
+            if (element.Length <= 3)
+            {
+                finalArray[resultIndex] = element;
+                resultIndex++;
+            }
         }
     }
-}
