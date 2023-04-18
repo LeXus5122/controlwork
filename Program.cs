78790c46f8a2fe2305822e61f0c312d9fb88376e
LeXus22 <lexus22@inbox.ru>
2023-04-18 10:34:08 +0300

    first version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] inputArray = new string[] { "Hello", "2", "world", ":-)" };
+int arraySize = inputArray.Length;
+    string[] userInputArray = CreateUserArray(arraySize);
+    Array.Copy(inputArray, userInputArray, arraySize);
+    Console.WriteLine("Входной массив:");
+    DisplayArray(userInputArray);
+    int finalArraySize = CalculateResultArraySize(userInputArray);
+    string[] finalArray = CreateUserArray(finalArraySize);
+    FillResultantArray(userInputArray, finalArray);
+    Console.WriteLine("Результирующий массив:");
+    DisplayArray(finalArray);
+
+
+static string[] CreateUserArray(int size)
+{
+    string[] newArray = new string[size];
+    return newArray;
+}
+
+static void DisplayArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+    Console.WriteLine();
+}
+
+static int CalculateResultArraySize(string[] array)
+{
+    int resultSize = 0;
+    foreach (string element in array)
+    {
+        if (element.Length <= 3) resultSize++;
+    }
+    return resultSize;
+}
+
+static void FillResultantArray(string[] initialArray, string[] finalArray)
+{
+    int resultIndex = 0;
+    foreach (string element in initialArray)
+    {
+        if (element.Length <= 3)
+        {
+            finalArray[resultIndex] = element;
+            resultIndex++;
+        }
+    }
+}
